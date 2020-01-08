8f5ad018d4ef2169624350e0eefac62a644ba870
TechDweebGaming <TechDweebGaming@users.noreply.github.com>
2020-01-08 09:30:21 -0800

    added num and parameter stuff

---
+++ src/Num.java
@@ -0,0 +1,35 @@
+
+
+
+public class Num {
+    private int value;
+
+    public Num(int value) {//constructor
+        this.value = value;
+    }
+
+
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+   public String toString(){
+      return "" + value;
+
+   }
+
+   public boolean equals(Num otherNum){
+        return this.value == otherNum.value;
+
+   }
+
+
+
+
+
+}
